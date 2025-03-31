c964661bfd1c3046c19668010830800ddc04a958
Jan Palička <jpalicka27@gmail.com>
2025-03-31 16:21:24 +0200

    feat: fixup not incoming data issue

--- src/main/java/jpWeatherStation/Main.java
+++ src/main/java/jpWeatherStation/Main.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        ConfigLoader config = new ConfigLoader();
         try {
-            new SilentHttpServerDataGetter(config);
+            new SilentHttpServerDataGetter();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
